afc6d39e2dcfb56b2eb354b9cccfd3da09768bc5
Thanh Nguyen <thanhnguyen2187@gmail.com>
2021-07-05 00:18:11 +0700

    Fix update script

--- scripts/update-site-remote.sh
+++ scripts/update-site-remote.sh
@@ -3,7 +3,7 @@
 ssh -tt "$USER@$HOST" << ENDSSH
 cd $ISLET_DIR
 git pull
-git submodule update --init
+git submodule update --remote
 just build-static-page
 docker-compose up -d --force-recreate 
 ENDSSH
